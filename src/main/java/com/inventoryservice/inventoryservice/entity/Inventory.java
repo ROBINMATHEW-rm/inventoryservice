4a097dbecb016d3660bf68b64156f17c245e4bf2
Robin Mathew <robin.mathew619619@gmail.com>
2026-08-23 00:00:13 +0530

    changes

---
+++ src/main/java/com/inventoryservice/inventoryservice/entity/Inventory.java
@@ -0,0 +1,23 @@
+package com.inventoryservice.inventoryservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "inventory")
+public class Inventory {
+
+    @Id
+    private String id;
+
+    private String productId;
+
+    private Integer availableQuantity;
+
+    private Integer reservedQuantity;
+}
--- src/test/java/com/inventoryservice/inventoryservice/CustomerserviceApplicationTests.java
+++ src/test/java/com/inventoryservice/inventoryservice/CustomerserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.customerservice.customerservice;
+package com.inventoryservice.inventoryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
